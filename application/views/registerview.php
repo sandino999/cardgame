d3b467ae0ee8d54ff00baa2028220eb379c020b7
sandino dolosa <sandino_dolosa@yahoo.com>
2012-10-12 13:55:17 +0800

    added client side validation

--- application/views/registerview.php
+++ application/views/registerview.php
@@ -8,40 +8,70 @@ include('header.php');
 			Register 
 		</title>
 		<link href="<?php echo url::base(); ?>bootstrap/css/bootstrap.css" rel="stylesheet">
+		
 	</head>
 	<body>
 		<div style="position:relative; top:200;height:0px;left:700">
 			<div class="container-fluid">  	
 			  <div class="row-fluid"> .
-				  <div class="span3">  
+				  <div class="span4">  
 				    <div class="well sidebar-nav"> 
-<?php
 			
-			echo "	<h1> Fill in the necessary fields </h1>";
-			echo '<font color =red>'.$message.'</font>';
-			echo form::open('accounts/confirm',array('method'=>'post'));
-			echo 'First Name '.form::input('txtfname').'<br/>';
-			echo 'Middle Name '.form::input('txtmname').'<br/>';
-			echo 'Last Name '.form::input('txtlname').'<br/>';
-			echo 'username '.form::input('txtuser').'<br/>';
-			echo 'password '.form::password('txtpass').'<br/>';
-			echo 'retype password'.form::password('txtretype').'<br/>';
-			echo 'secret question'.form::select('secret_question',array('','What is your pet\'s name',
-																'What was your childhood nickname?',
-																'What is your oldest cousin\'s first and last name?',
-																'In what city or town was your first job?',
-																'What is the name of a college you applied to but didn\'t attend?',
-																'Who was your childhood hero?',
-																'What was your favorite sport in high school?',
-																'What is your spouse\'s mother\'s maiden name?'
-																	)).'<br/>';
-			echo 'answer'.form::input('txtans').'<br/>';
-			echo 'Contact No '.form::input('txtcontact').'<br/>';
-			echo 'Address '.form::input('txtaddr').'<br/>';
-			echo 'Email Address '.form::input('txtemail').'<br/>';
-			?><input type='submit' value='Register' name='btnregistertodatabase' class='btn btn-primary' ><?php
-			echo form::close();
-		
+			<h1> Fill in the necessary fields </h1>
+			<font color='red'><span id='return_error'></span></font>
+			<font color ='red'><?php echo $message ?></font>
+			<form action='confirm' method='post' id='register'>
+				First Name 	<input type='text' name='txtfname' id='fname'>
+							<font color='red'><span id='return_fname'></span></font><br/>
+				Middle Name <input type='text' name='txtmname' id='mname'>
+							<font color='red'><span id='return_mname'></span></font><br/>
+				Last Name 	<input type='text' name='txtlname' id='lname'>
+							<font color='red'><span id='return_lname'></span></font><br/>
+				username 	<input type='text' name='txtuser' id='username'>
+							<font color='red'><span id='return_username'></span></font><br/>
+				password 	<input type='password' name='txtpass' id='password'>
+							<font color='red'><span id='return_password'></span></font><br/>
+				retype password<input type='password' name='txtretype' id='retype'>
+								<font color='red'><span id='return_retype'></span></font><br/>		
+				secret question <select name='secret_question' id='select'>
+									<option>
+										What is your pet's name
+									</option>
+									<option>
+										What was your childhood nickname?
+									</option>
+									<option>
+										What is your oldest cousin's first and last name?
+									</option>
+									<option>
+										In what city or town was your first job?
+									</option>
+									<option>
+										What is the name of a college you applied to but didn't attend?
+									</option>
+									<option>
+										Who was your childhood hero?
+									</option>
+									<option>
+										What was your favorite sport in high school?
+									</option>
+									<option>
+										What is your spouse's mother's maiden name?
+									</option>
+								</select>
+							<br/>
+				answer 	<input type='text' name='txtans' id='ans'>
+						<font color='red'><span id='return_ans'></span></font><br/>	
+				Contact No 	<input type='text' name='txtcontact' id='contact'>
+							<font color='red'><span id='return_contact'></span></font><br/>
+				Address <input type='text' name='txtaddr' id='addr'>
+						<font color='red'><span id='return_addr'></span></font><br/>
+				Email Address 	<input type='text' name='txtemail' id='email'>
+								<font color='red'><span id='return_email'></span></font><br/>
+				<input type='submit' value='Register' name='btnregistertodatabase' class='btn btn-primary' id='btnreg'>
+			</form>
+			
+		<?php
 			echo "<div style='position:relative; top:-50;left:85'>";
 			echo form::open('mycardlist',array('method'=>'post'));
 			?><input type='submit' value='Back' name='btnback' class='btn btn-primary' ><?php
@@ -53,6 +83,8 @@ include('header.php');
 				</div>
 			</div>
 		</div>
+	  <script src="http://code.jquery.com/jquery-latest.js"></script>
+	<script src="<?php echo url::base() ?>bootstrap/js/validate.js"></script>
 	</body>
 </html>
 <?php	
---
+++ bootstrap/js/validate.js
@@ -0,0 +1,354 @@
+// firstname validation
+	
+	var error=0
+	
+	$("#fname").change( function() {
+		var value = $('#fname').val();
+	
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_fname').html(error_message);
+			
+		}
+		else if(value.length < 2)
+		{
+			error =1;
+			error_message = 'Please enter atleast 2 characters';
+			$('#return_fname').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_fname').html(error_message);
+		}
+	});
+  
+// middlename validation
+
+	$("#mname").change( function() {
+		var value = $('#mname').val();
+	
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_mname').html(error_message);
+		}
+		else if(value.length < 2)
+		{
+			error =1;
+			error_message = 'Please enter atleast 2 characters';
+			$('#return_mname').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_mname').html(error_message);
+		}
+	});
+
+// lastname validation
+
+	$("#lname").change( function() {
+		var value = $('#lname').val();
+	
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_lname').html(error_message);
+		}
+		else if(value.length < 2)
+		{
+			error =1;
+			error_message = 'Please enter atleast 2 characters';
+			$('#return_lname').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_lname').html(error_message);
+		}
+	});
+	
+//username validation
+  
+	$("#username").change( function() {
+			var value = $('#username').val();
+		
+			if(jQuery.trim(value) == '')
+			{
+				error =1;
+				error_message = 'This Field is required';
+				$('#return_username').html(error_message);
+			}
+			else if(value.length < 2)
+			{
+				error =1;
+				error_message = 'Please enter atleast 2 characters';
+				$('#return_username').html(error_message);
+			}
+			else
+			{
+				error =0;
+				error_message = '';
+				$('#return_username').html(error_message);
+			}
+		});
+
+//password validation  
+  
+	$("#password").change( function() {
+		var value = $('#password').val();
+		
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_password').html(error_message);
+		}
+		else if(value.length < 8)
+		{
+			error =1;
+			error_message = 'Password must be 8 characters long';
+			$('#return_password').html(error_message);
+		}
+		else if($('#retype').val() == '')
+		{
+			error =1;
+			error_message = 'Enter retype password';
+			$('#return_password').html(error_message);
+		}
+		else if(value != $('#retype').val())
+		{
+			error =1;
+			error_message = 'Password does not match';
+			$('#return_password').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			error_message2 = ''
+			$('#return_password').html(error_message);
+			$('#return_retype').html(error_message2);
+		}
+	});
+	
+// retype password validation
+
+	$("#retype").change( function() {
+		var value = $('#retype').val();
+			
+		if(value != $('#password').val())
+		{
+			error =1;
+			error_message = 'Password does not match';
+			$('#return_password').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_password').html(error_message);
+		}
+	});	
+	
+//sercret question answer validation
+
+ 
+	$("#ans").change( function() {
+		var value = $('#ans').val();
+		
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_ans').html(error_message);
+		}
+		else if(value.length < 2)
+		{
+			error =1;
+			error_message = 'Please enter atleast 2 characters';
+			$('#return_ans').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_ans').html(error_message);
+		}
+	});
+		
+// contact number validation
+
+	$("#contact").change( function() {
+		
+		var value = $('#contact').val();
+		var numberRegex = /^[+-]?\d+(\.\d+)?([eE][+-]?\d+)?$/;
+		
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_contact').html(error_message);
+		}
+		else if(!numberRegex.test(value))
+		{
+			error =1;
+			error_message = 'Not a number';
+			$('#return_contact').html(error_message);
+		}
+		else if(value.length < 7)
+		{
+			error =1;
+			error_message = 'Please enter atleast 7 characters';
+			$('#return_contact').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_contact').html(error_message);
+		}
+	});
+
+//address validation
+
+	$("#addr").change( function() {
+		var value = $('#addr').val();
+		
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_addr').html(error_message);
+		}
+		else if(value.length < 2)
+		{
+			error =1;
+			error_message = 'Please enter atleast 2 characters';
+			$('#return_addr').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_addr').html(error_message);
+		}
+	});	
+	
+//validate email address
+	
+	$("#email").change( function() {
+		
+		var value = $('#email').val();
+		var email_check = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}$/i;
+		
+		if(jQuery.trim(value) == '')
+		{
+			error =1;
+			error_message = 'This Field is required';
+			$('#return_email').html(error_message);
+		}	
+		else if(!email_check.test(value))
+		{	
+			error =1;
+			error_message = 'Not a valid email';
+			$('#return_email').html(error_message);
+		}
+		else
+		{
+			error =0;
+			error_message = '';
+			$('#return_email').html(error_message);
+		}
+	});	
+	
+// validate submit
+
+	$('#register').submit(function() {
+		if(error!=0)
+		{
+			error_message = 'There were problems in creating your accounts, check all the details in the input field';
+				$('#return_error').html(error_message);
+			return false;
+		}
+		else if(jQuery.trim($('#fname').val()) == '' || jQuery.trim($('#mname').val()) == '' || jQuery.trim($('#lname').val()) == ''
+				|| jQuery.trim($('#username').val()) == '' || jQuery.trim($('#password').val()) == '' || jQuery.trim($('#retype').val()) == ''
+				|| jQuery.trim($('#ans').val()) == '' || jQuery.trim($('#contact').val()) == '' || jQuery.trim($('#addr').val()) == '' || jQuery.trim($('#email').val()) == '')
+		{
+			if(jQuery.trim($('#fname').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_fname').html(error_message);
+			}
+			 
+			if(jQuery.trim($('#mname').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_mname').html(error_message);
+			}
+			
+			if(jQuery.trim($('#lname').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_lname').html(error_message);
+			}
+			
+			if(jQuery.trim($('#username').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_username').html(error_message);
+			}
+			
+			if(jQuery.trim($('#password').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_password').html(error_message);
+			}
+			
+			if(jQuery.trim($('#retype').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_retype').html(error_message);
+			}
+			
+			if(jQuery.trim($('#ans').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_ans').html(error_message);
+			}
+			
+			if(jQuery.trim($('#contact').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_contact').html(error_message);
+			}
+			
+			if(jQuery.trim($('#addr').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_addr').html(error_message);
+			}
+			
+			if(jQuery.trim($('#email').val()) == '')
+			{
+				error_message = 'This field is required';
+				$('#return_email').html(error_message);
+			}
+			
+			return false;
+		}
+		else
+		{
+			return true;
+		}
+	});
+
+	
